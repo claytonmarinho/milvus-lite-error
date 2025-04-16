fd4a6420feff7e0e758f73416d9b66eb2b702529
Clayton <claytonmarinho@gmail.com>
2025-04-16 13:01:07 -0300

    amend python

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from pymilvus import MilvusClient
+from pymilvus import MilvusClient, DataType
 
 import threading
 
 print(f"THREADS >>>>>>>>>>>>>>>: {threading.active_count()}")
 client = MilvusClient("./data/milvus_test.db")
 
+schema = MilvusClient.create_schema(
+        auto_id=False,
+        enable_dynamic_field=True,
+    )
+    # Add fields to schema
+schema.add_field(field_name="id", datatype=DataType.VARCHAR, is_primary=True, max_length=50) # SYNONYM_ID
+print("✅ Done: Preparing index params...")
 
+print("Done!")
