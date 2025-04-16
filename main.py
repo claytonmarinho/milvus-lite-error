381b487fe185d5cb9dfad3ca6864d9d62d481706
Clayton Marinho <claytonmarinho@gmail.com>
2025-04-16 13:11:41 -0300

    fixed

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+import os
 from pymilvus import MilvusClient, DataType
 
 import threading
 
+data_dir = "./data-local"
+
+if not os.path.exists(data_dir):
+    os.makedirs(data_dir)
+    print("Created data-local dir")
+
 print(f"THREADS >>>>>>>>>>>>>>>: {threading.active_count()}")
-client = MilvusClient("./data/milvus_test.db")
+client = MilvusClient(f"{data_dir}/milvus_test.db")
 
 schema = MilvusClient.create_schema(
         auto_id=False,
